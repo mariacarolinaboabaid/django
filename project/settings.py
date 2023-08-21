d7e18a81373b8f3e686ec8e009de68272662b9df
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-08-21 09:16:17 -0300

    commit

--- project/settings.py
+++ project/settings.py
@@ -87,13 +87,13 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# import os
-# DATABASE_URL =  os.getenv('DATABASE_URL')
-# if DATABASE_URL:
-#     DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-# }
+import dj_database_url
+import os
+DATABASE_URL =  os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
