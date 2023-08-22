5397cc401d50cb7813789fad16398e0a652a2df1
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-08-22 19:10:38 -0300

    commit

--- project/settings.py
+++ project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 import dj_database_url
 import os
-DATABASE_URL =  os.getenv('DATABASE_URL')
+DATABASE_URL =  "postgresql://postgres:x7iVsjBlBd37yCwJbJb2@containers-us-west-59.railway.app:6109/railway"
 if DATABASE_URL:
     DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
